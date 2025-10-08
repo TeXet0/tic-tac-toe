2a3aa4aca76e2e4ebfbcf5118e0febb0c3aa9bf9
TeXet0 <dankruk06@gmail.com>
2025-10-09 01:21:41 +0300

    Create reusable Layout and Button components

---
+++ src/components/UI/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from 'Layout.module.scss'
+
+const Layout = ({ children }) => {
+    return (
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <h1>Гра "Хрестики-Нулики"</h1>
+            </header>
+            <main>
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
