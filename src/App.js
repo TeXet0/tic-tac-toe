723945b97475e0c8d1189575641f2badbdef5d7f
TeXet0 <dankruk06@gmail.com>
2025-10-22 13:27:52 +0300

    Implement app-level state and page navigation in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import GamePage from './pages/GamePage/GamePage';
+import React, { useState, useCallback } from 'react';
 import StartPage from './pages/StartPage/StartPage';
+import GamePage from './pages/GamePage/GamePage';
 import ResultsPage from './pages/ResultsPage/ResultsPage';
 import './assets/styles/main.scss';
 
 function App() {
-  return (
-      <div className="App">
-        <GamePage />
-        {/* <StartPage /> */}
-        {/* <ResultsPage /> */}
-      </div>
-  );
+    const [currentPage, setCurrentPage] = useState('start');
+    const [gameResult, setGameResult] = useState(null);
+
+    const handleStartGame = () => {
+        setGameResult(null);
+        setCurrentPage('game');
+    };
+
+    const handleEndGame = useCallback((result) => {
+        setGameResult(result); // 'X', 'O' або 'Draw'
+        setCurrentPage('results');
+    }, []);
+
+    const handleRestart = () => {
+        setCurrentPage('start');
+    };
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'game':
+                return <GamePage onGameEnd={handleEndGame} />;
+            case 'results':
+                return <ResultsPage winner={gameResult} onRestart={handleRestart} />;
+            case 'start':
+            default:
+                return <StartPage onStartGame={handleStartGame} />;
+        }
+    };
+
+    return <div className="App">{renderPage()}</div>;
 }
 
 export default App;
