a65a06a756b3f62fdb556aa776239baff85b56f0
TeXet0 <dankruk06@gmail.com>
2025-11-28 20:46:56 +0200

    Remove SettingsContext and provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './assets/styles/main.scss';
-import { SettingsProvider } from './context/SettingsContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
-            <SettingsProvider>
-                <App />
-            </SettingsProvider>
+            <App />
         </BrowserRouter>
     </React.StrictMode>,
 )
